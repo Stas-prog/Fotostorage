59f887b3c8ce2d5c2cc3b064b9dcc19f5aa932af
Stas-prog <skubav86@gmail.com>
2025-04-25 02:54:07 +0300

    next4 commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const nextConfig = {
     env: {
         MONGODB_URI: process.env.MONGODB_URI,
         API_URL: process.env.API_URL,
-        UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN
+        UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
+        UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+        UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID
+
     },
     images: {
         remotePatterns: [
