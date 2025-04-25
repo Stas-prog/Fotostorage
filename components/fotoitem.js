2080d15504b345678092a37049553dcc36e6b041
Stas-prog <skubav86@gmail.com>
2025-04-25 22:52:13 +0300

    next6 commit

--- components/fotoitem.js
+++ components/fotoitem.js
@@ -7,8 +7,8 @@ const FotoItem = observer(({ onClick, foto }) => {
     const removeFoto = async () => { await deleteFoto(foto._id) }
     return (
         <Col md={3} className={"mt-3"} >
-            <Card style={{ width: 210, cursor: 'pointer', backgroundColor: '#AFDDFF' }} border={"light"}>
-                <Image style={{ objectFit: 'contain' }} onClick={onClick} width={205} height={190} src={foto.img} alt='tyur' />
+            <Card style={{ width: 220, cursor: 'pointer', backgroundColor: '#AFDDFF' }} border={"light"}>
+                <Image style={{ objectFit: 'contain' }} onClick={onClick} width={215} height={190} src={foto.img} alt='tyur' />
                 <Button style={{ width: 1, height: 1, backgroundColor: '#9ACBD0', border: 'none' }} onClick={removeFoto}></Button>
             </Card>
         </Col>
